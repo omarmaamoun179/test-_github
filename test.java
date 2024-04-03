db5bf836ef517be05f08b07016cd236c849bbffc
omarmaamoun <omarmaamoun6@gmail.com>
2024-04-03 10:07:20 +0200

    omar java code

---
+++ test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class GeeksforGeeks {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter the name of the student: ");
+        String user_name = Sc.nextLine();
+        System.out.println(user_name);
+    }
+}
